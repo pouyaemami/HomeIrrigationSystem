b79980853bba5ddeb3634e38f27cb61551d2ef9c
Pouya Emami <pouyaemami@gmail.com>
2026-01-04 19:34:39 +0100

    Implement RTC and Time Sync

--- src/main-parent.cpp
+++ src/main-parent.cpp
@@ -2,20 +2,23 @@
 #include <SPI.h>
 #include <WiFi101.h>
 #include <DHT.h>
+#include <NTPClient.h>
+#include <RTCZero.h>
+#include "time.h"
 
 #include "config.h"
 #include "Web.h"
 #include "MDNS.h"
 #include "DeviceManagement.h"
 
-// int status = WL_IDLE_STATUS; // the Wifi radio's status
-
 WiFiServer server(80);
 WiFiClient client = server.available();
 MDNS mdns;
 DeviceManagement deviceManager;
 
-int ledPin = LED_BUILTIN;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+RTCZero rtc;
 
 #define DHTPIN 0      // Pin which is connected to the DHT sensor
 #define DHTTYPE DHT22 // DHT 22 (AM2302)
@@ -23,6 +26,17 @@ DHT dht(DHTPIN, DHTTYPE);
 
 float temperature = 0.0;
 float humidity = 0.0;
+
+void setRTCFromNTP()
+{
+  timeClient.update();
+  time_t rawtime = timeClient.getEpochTime();
+  struct tm *ti;
+  ti = localtime(&rawtime);
+
+  rtc.setTime(ti->tm_hour, ti->tm_min, ti->tm_sec);
+  rtc.setDate(ti->tm_mday, ti->tm_mon + 1, ti->tm_year - 100);
+}
 
 void setup()
 {
@@ -37,10 +51,17 @@ void setup()
   deviceManager.setup();
 
   dht.begin();
+
+  // start the NTP client
+  timeClient.begin();
+  rtc.begin();
+
+  setRTCFromNTP();
 }
 
 void loop()
 {
+  timeClient.update();
   mdns.poll();
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
